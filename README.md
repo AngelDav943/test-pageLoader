cdad82524d5c3f3041508c8c69906c8e5b7d4dbb
Angel <angel91.addcs@gmail.com>
2024-07-25 08:04:06 -0500

    updated the module version to latest version of pageloader

--- README.md
+++ README.md
@@ -1,20 +1,163 @@
-# test-pageLoader package
-this package uses angeldav-testpackage and express
+# pageLoader package
+This is a small package i made that handles all express's app.get requests, it uses angeldav-loaderhtml and expressjs.
 
-example of nodejs server
+Initalization variables for ``angeldav-loaderhtml``
+```javascript
+// import angeldav-loaderhtml
+const page = require('angeldav-loaderhtml');
+
+// set url of the website to replace __rooturl in the html files to the chosen url
+page.url = "localhost:1234"
+
+// Page to show when a page is not found
+page.default.notfound = `${__dirname}/notfound.html`
+
+// Base template for the pages
+page.default.template = `${__dirname}/template.html`
+
+// OPTIONAL
+// replaces tags like <¡foo> to the content inside this value in all pages
+page.default.other = {
+    "foo":"<input type='button' value='button'>"
+}
+```
+
+page loader
 ```javascript
 const express = require('express');
 const app = express();
-const package = require('angeldav-testpackage');
 
-package.url = "http://localhost:3000"
-package.default.template = `${__dirname}/template.html`
-package.default.notfound = `${__dirname}/view/404.html`
+const pageloader = require(`angeldav-pageloader`)(page,{
+    "app":app, // express app
+    "path":`${__dirname}/view` // folder containing all pages to load
+})
+```
+
+## TAGS
+
+404 error tags
+```html
+<¡errortitle> <!-- Displays error title example: 404: Page not found -->
+<¡errormessage> <!-- Displays error message ex: {page name} isn't a valid page -->
+<¡errorcode> <!-- Displays error code ex: 404 -->
+```
+
+Other tags
+```html
+__pagetitle  <!-- Displays the title chosen in the config table -->
+__rooturl  <!-- Returns the website url stated in package.url -->
+```
+
+## EXAMPLES
+
+Example of ``index.js``
+```javascript
+const express = require('express');
+const app = express();
+const page = require('angeldav-loaderhtml');
+
+page.url = "http://localhost:3000"
+page.default.template = `${__dirname}/template.html`
+page.default.notfound = `${__dirname}/view/404.html`
 
 app.use('/public', express.static('public'));
 
-const pageloader = require(`angeldav-test-pageloader`)(package,{
+const pageloader = require(`angeldav-pageloader`)(page,{
     "app":app,
     "path":`${__dirname}/view`
 })
+```
+
+Example of ``template.html``
+```html
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Website</title>
+    </head>
+
+    <body>
+        <section class="<¿templatesectionclass>">
+            <¿templatesectionmain> <!-- required for loader content to show -->
+        </section>
+    </body>
+
+</html>
+```
+Example of index page
+
+``view/index.html``
+```html
+<p>Hello world!</p>
+```
+
+![screenshot](https://github.com/AngelDav943/test-pageLoader/assets/35638964/99e25d26-948d-4b7e-959c-2c6f505c79e3)
+
+Example of static page
+
+``view/test.html``
+```html
+<h2>Test</h2>
+<p>Hello this is a test</p>
+```
+
+![screenshot1](https://github.com/AngelDav943/test-pageLoader/assets/35638964/87726d8c-7f87-4b61-8357-1424b118005a)
+
+Example of dynamic page
+
+``view/codetest.js``
+```javascript
+module.exports = function(req, res, page) {
+    const number1 = parseInt(req.query.a)
+    const number2 = parseInt(req.query.b)
+    var total = number1 + number2
+
+    new page.loader({
+        "res": res,
+        "req": req,
+        "title": "Sum of numbers",
+        "templatedir": `${__dirname}/../../pages/codetest.html`,
+        "other": {
+            "num1": number1,
+            "num2": number2,
+            "total": total
+        }
+    }).load()
+}
+```
+
+``pages/codetest.html``
+```html
+<h2>__pagetitle</h2>
+<p>The sum of <¡num1> and <¡num2> is <¡total></p>
+```
+
+![screenshot](https://github.com/AngelDav943/test-pageLoader/assets/35638964/b4f58788-4bc1-4d14-97c3-a83d6661bc02)
+
+## page.default.preload
+``angeldav-testpackage`` stuff.
+
+``page.default.preload`` sets javascript code to run before fully loading a page, it lets you modify the template page before any more changes with the ``base`` variable.
+
+Useful when adding logged in username, custom themes and other stuff.
+
+### example usage of preload
+``index.js``
+```javascript
+const page = require('angeldav-loaderhtml');
+page.preload = `${__dirname}/default_preload.js`
+```
+``default_preload.js``
+```javascript
+function getCookie(cookie, name) {
+    if (cookie.includes(name+"=") == false || name == "") return ""
+    var result = cookie.slice(cookie.indexOf(name))
+    if (result.includes(";")) result = result.slice(0, result.indexOf(";"))
+    result = result.split("=")
+    return result[result.length-1]
+}
+
+let theme = getCookie(req.headers.cookie, "theme")
+if (theme == "special") base = fs.readFileSync(`${__dirname}/../../templates/special.html`).toString()
 ```
--- index.js
+++ index.js
@@ -1,88 +1,87 @@
-const fs = require("fs");
-let page = require("angeldav-loaderhtml");
+const fs = require('fs');
+let page = require('angeldav-loaderhtml');
 
-module.exports = async function (testpage, config) {
+module.exports = async function(testpage,config) {
     if (testpage != null) page = testpage;
 
     async function loadQuery(req, res) {
-        let url = req.path.substring(1).split("/");
+        let url = req.path.substring(1).split('/');
         let urlpath = req.url;
-        urlpath = urlpath.split("?")[0];
+        urlpath = urlpath.split('?')[0];
 
         let filepath = `${urlpath.toLowerCase()}/index`;
-        if (url[0] == "") urlpath = "/index";
+        if (url[0] == "") urlpath = "/index"
 
         if (fs.existsSync(`${config.path}${urlpath.toLowerCase()}.html`)) {
-            filepath = `${config.path}${urlpath.toLowerCase()}.html`;
-        } else if (
-            fs.existsSync(`${config.path}${urlpath.toLowerCase()}/index.html`)
-        ) {
-            filepath = `${config.path}${urlpath.toLowerCase()}/index.html`;
+            filepath = `${config.path}${urlpath.toLowerCase()}.html`
+        } else if (fs.existsSync(`${config.path}${urlpath.toLowerCase()}/index.html`)) {
+            filepath = `${config.path}${urlpath.toLowerCase()}/index.html`
         }
 
         let title = url[url.length - 1];
         let scriptpath = `${config.path}${urlpath.toLowerCase()}.js`;
-        let scriptloaded = false;
+        let scriptloaded = false
 
         Object.keys(require.cache).forEach(key => {
             if (key.includes("node_modules")) return
-            console.log("not a node, remove cache...")
             delete require.cache[key]
         })
 
-        const testpath = scriptpath.replace(/\//g,"\\").replace(/\\/g,"\\\\")
-        console.log("CACHE:")
-        console.log(Object.keys(require.cache))
-        console.log("compare:")
-        console.log(testpath)
-        console.log(`isCached: ${Object.keys(require.cache).includes(testpath)}`)
+        try {
 
-        if (fs.existsSync(scriptpath)) {
-            var dirpath = `${config.path}${urlpath
-                .toLowerCase()
-                .substring(0, urlpath.length - url[url.length - 1].length)}`;
-            //
-            await require(scriptpath)(req, res, page);
-            // eval(fs.readFileSync(scriptpath).toString())
-
-            scriptloaded = true;
-        } else {
-            scriptpath = `${config.path}/${urlpath.toLowerCase()}/index.js`;
-            if (urlpath.toLowerCase() == "/")
-                scriptpath = `${config.path}/index.js`;
-            try {
-                //
+            if (fs.existsSync(scriptpath)) {
+                let dirpath = `${config.path}${urlpath.toLowerCase().substring(0, urlpath.length - url[url.length-1].length)}`
                 await require(scriptpath)(req, res, page);
                 // eval(fs.readFileSync(scriptpath).toString())
-                scriptloaded = true;
-                return;
-            } catch (err) {
-                try {
-                    if (
-                        urlpath.toLowerCase() != "/" &&
-                        url[0].toLowerCase() != "assets"
-                    ) {
-                        //
+                scriptloaded = true
+            } else {
+                scriptpath = `${config.path}/${urlpath.toLowerCase()}/index.js`
+                if (urlpath.toLowerCase() == "/") scriptpath = `${config.path}/index.js`
+
+                if (fs.existsSync(scriptpath)) {
+                    // eval(fs.readFileSync(scriptpath).toString())
+                    await require(scriptpath)(req, res, page);
+                    scriptloaded = true
+                    // return
+                } else if (urlpath.toLowerCase != "/" && url[0].toLowerCase() != "assets") {
+                    scriptpath = `${config.path}/${url[0].toLowerCase()}/notfound.js`
+
+                    console.log(urlpath.toLowerCase())
+                    if (fs.existsSync(scriptpath)) {
                         await require(scriptpath)(req, res, page);
-                        // eval(fs.readFileSync(`${config.path}/${url[0].toLowerCase()}/notfound.js`).toString())
-                        scriptloaded = true;
-                        return;
+                        // eval(fs.readFileSync(scriptpath).toString())
+                        scriptloaded = true
                     }
-                } catch (err) {}
+                }
             }
+        } catch (error) {
+            console.error(error);
         }
 
-        if (!scriptloaded) {
-            new page.loader({
-                res: res,
-                req: req,
-                title: title,
-                templatedir: filepath,
-            }).load();
-        }
+        if (!scriptloaded) new page.loader({
+            "res":res,
+            "req":req,
+            "title":title,
+            "templatedir":filepath
+        }).load()
     }
 
-    config.app.get("*", (req, res) => {
+    config.app.get('*', (req, res) => {
         loadQuery(req, res);
     });
-};
+
+    config.app.use(function(error, req, res, next) {
+        new page.loader({
+            "res":res,
+            "req":req,
+            "title":"ERROR 500",
+            "templatedir":page.default.notfound,
+            "other":{
+                "errortitle": `Internal Server Error`,
+                "errorcode": `500`,
+                "errormessage": error
+            }
+        }).load()
+        console.error(error);
+    });
+}
